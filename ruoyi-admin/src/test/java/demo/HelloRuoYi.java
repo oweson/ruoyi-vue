8763c62f16fa1d6946ffb5c9008b61a8e148b299
chengwanli <chengwanli@qq.com>
2021-01-17 23:01:47 +0800

    走向未来，hello!

---
+++ ruoyi-admin/src/test/java/demo/HelloRuoYi.java
@@ -0,0 +1,13 @@
+package demo;
+
+/**
+ * @author oweson
+ * @date 2021/1/17 23:00
+ */
+
+
+public class HelloRuoYi {
+    public static void main(String[] args) {
+        System.out.println("前后端分离是大趋势！");
+    }
+}
