e7ba6a1c46d2e5eae635c82c66489db79149c3ed
oweson <5703477202@qq.com>
2021-09-20 09:29:04 +0800

    add pom

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/PrivateMobileManagephoneServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/PrivateMobileManagephoneServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ruoyi.system.service.impl;
 
 import java.util.List;
+
+import com.alibaba.fastjson.JSON;
 import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.redis.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruoyi.system.mapper.PrivateMobileManagephoneMapper;
@@ -10,87 +13,91 @@ import com.ruoyi.system.service.IPrivateMobileManagephoneService;
 
 /**
  * 手机号码Service业务层处理
- * 
+ *
  * @author ruoyi
  * @date 2021-04-05
  */
 @Service
-public class PrivateMobileManagephoneServiceImpl implements IPrivateMobileManagephoneService 
-{
+public class PrivateMobileManagephoneServiceImpl implements IPrivateMobileManagephoneService {
     @Autowired
     private PrivateMobileManagephoneMapper privateMobileManagephoneMapper;
+    @Autowired
+    RedisUtil redisUtil;
 
     /**
      * 查询手机号码
-     * 
+     *
      * @param id 手机号码ID
      * @return 手机号码
      */
     @Override
-    public PrivateMobileManagephone selectPrivateMobileManagephoneById(Integer id)
-    {
-        return privateMobileManagephoneMapper.selectPrivateMobileManagephoneById(id);
+    public PrivateMobileManagephone selectPrivateMobileManagephoneById(Integer id) {
+        PrivateMobileManagephone managephone = null;
+        String phone = (String)redisUtil.get(id + "");
+        if (phone == null) {
+            managephone = privateMobileManagephoneMapper.selectPrivateMobileManagephoneById(id);
+            redisUtil.set(id+"", JSON.toJSONString(managephone,true));
+        } else {
+         managephone=  JSON.parseObject(phone,PrivateMobileManagephone.class);
+        }
+        return managephone;
     }
 
     /**
      * 查询手机号码列表
-     * 
+     *
      * @param privateMobileManagephone 手机号码
      * @return 手机号码
      */
     @Override
-    public List<PrivateMobileManagephone> selectPrivateMobileManagephoneList(PrivateMobileManagephone privateMobileManagephone)
-    {
+    public List<PrivateMobileManagephone> selectPrivateMobileManagephoneList(PrivateMobileManagephone privateMobileManagephone) {
+
         return privateMobileManagephoneMapper.selectPrivateMobileManagephoneList(privateMobileManagephone);
     }
 
     /**
      * 新增手机号码
-     * 
+     *
      * @param privateMobileManagephone 手机号码
      * @return 结果
      */
     @Override
-    public int insertPrivateMobileManagephone(PrivateMobileManagephone privateMobileManagephone)
-    {
+    public int insertPrivateMobileManagephone(PrivateMobileManagephone privateMobileManagephone) {
         privateMobileManagephone.setCreateTime(DateUtils.getNowDate());
         return privateMobileManagephoneMapper.insertPrivateMobileManagephone(privateMobileManagephone);
     }
 
     /**
      * 修改手机号码
-     * 
+     *
      * @param privateMobileManagephone 手机号码
      * @return 结果
      */
     @Override
-    public int updatePrivateMobileManagephone(PrivateMobileManagephone privateMobileManagephone)
-    {
+    public int updatePrivateMobileManagephone(PrivateMobileManagephone privateMobileManagephone) {
         privateMobileManagephone.setUpdateTime(DateUtils.getNowDate());
         return privateMobileManagephoneMapper.updatePrivateMobileManagephone(privateMobileManagephone);
     }
 
     /**
      * 批量删除手机号码
-     * 
+     *
      * @param ids 需要删除的手机号码ID
      * @return 结果
      */
     @Override
-    public int deletePrivateMobileManagephoneByIds(Integer[] ids)
-    {
+    public int deletePrivateMobileManagephoneByIds(Integer[] ids) {
         return privateMobileManagephoneMapper.deletePrivateMobileManagephoneByIds(ids);
     }
 
     /**
      * 删除手机号码信息
-     * 
+     *
      * @param id 手机号码ID
      * @return 结果
      */
     @Override
-    public int deletePrivateMobileManagephoneById(Integer id)
-    {
+    public int deletePrivateMobileManagephoneById(Integer id) {
         return privateMobileManagephoneMapper.deletePrivateMobileManagephoneById(id);
     }
 }
